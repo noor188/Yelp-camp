65c682dfdec1cfb3b4d22029df6786ab470b0616
Noor <noormmlk@yahoo.com>
2021-06-16 23:49:27 -0600

    create show page
    page to show specific camp

--- app.js
+++ app.js
@@ -26,10 +26,22 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/campgorunds", async (req, res) => {
+app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds });
+    // structure template in different folders 
+    res.render('campgrounds/index', { campgrounds })
+});
+
+app.get("/campgorunds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", { Campground });
 })
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({ title: "My Backyard", description: "cheap camping" });
+    await camp.save();
+    res.send(camp)
+});
 
 app.listen(3000, () => {
     console.log("Serving on port 3000");
